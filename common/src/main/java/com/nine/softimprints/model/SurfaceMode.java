ed1532aef32890879a3ece828b19693f22907cda
0Templ <155010222+0Templ@users.noreply.github.com>
2026-07-07 14:52:00 +0300

    small serialization fix

--- common/src/main/java/com/nine/softimprints/model/SurfaceMode.java
+++ common/src/main/java/com/nine/softimprints/model/SurfaceMode.java
@@ -4,6 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 public enum SurfaceMode {
 
+    @SerializedName(value = "overlay", alternate = {"OVERLAY"})
     OVERLAY,
     @SerializedName(value = "repaint", alternate = {"TOP", "top"})
     REPAINT;
